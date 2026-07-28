e015db95956de1ee077d1d4cbf76bfad47294203
Piyush yadav <piyush55.py@gmail.com>
2026-07-28 23:59:24 +0530

    Add files via upload

---
+++ Chapter 1/A0.c
@@ -0,0 +1,24 @@
+// A0 ki dimensions di hain 1189 mm X 841 mm hum A1,A2,A3,A4,A5,A6,A7,A8 k print krni hai 
+#include<stdio.h>
+int main (){
+    int h = 1189,w = 841;
+    printf("\nSize of A0 is %d X %d.",h,w);
+    int h2 = w, w2 = h/2;
+    printf("\nSize of A1 is %d X %d.",h2,w2);
+    int h3 = w2, w3 = h2/2;
+    printf("\nSize of A2 is %d X %d.",h3,w3);
+    int h4 = w3, w4 = h3/2;
+    printf("\nSize of A3 is %d X %d.",h4,w4);
+    int h5 = w4, w5 = h4/2;
+    printf("\nSize of A4 is %d X %d.",h5,w5);
+    int h6 = w5, w6 = h5/2;
+    printf("\nSize of A5 is %d X %d.",h6,w6);
+    int h7 = w6, w7 = h6/2;
+    printf("\nSize of A6 is %d X %d.",h7,w7);
+    int h8 = w7, w8 = h7/2;
+    printf("\nSize of A7 is %d X %d.",h8,w8);
+    int h9 = w8, w9 = h8/2;
+    printf("\nSize of A8 is %d X %d.",h9,w9);
+    return 0;
+    //5149
+}
